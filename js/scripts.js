991c99559e15b4ce0877f898c18acd79d3bbe0e3
David Zulaica and Mike Goren <mgoren@mortalwombat.net>
2015-02-16 10:50:59 -0800

    add delete ability

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $("form#new-place").submit(function(event) {
     event.preventDefault();
+    $("#show-place").hide();
 
     var location = $("input#location").val();
     var dates    = $("input#dates").val();
@@ -8,17 +9,24 @@ $(document).ready(function() {
 
     var newPlace = { location: location, dates: dates, notes: notes };
 
-    $("ul#places").append("<li><span class='place'>" + newPlace.location + "</span></li>");
+    $("ul#places").append("<li id='" + newPlace.location + "'><span class='place'>" + newPlace.location + "</span></li>");
 
     $("input#location").val("");
     $("input#dates").val("");
     $("input#notes").val("");
+    $(".deletable").empty();
 
     $(".place").last().click(function() {
       $("#show-place").show();
       $("#show-place h2").text(newPlace.location);
+      $(".deletable").html("<span class='delete'> [x]</span>");
       $(".dates").text(newPlace.dates);
       $(".notes").text(newPlace.notes);
+      $(".delete").click(function() {
+        $("#show-place").hide();
+        $("li#" + newPlace.location).remove();
+      });
+
     });
 
   });
